2e2be36468c1a957e8cc4d1c30b93aa693cc1631
Alex <thatsoalex@gmail.com>
2022-05-20 13:02:50 -0500

    Created Order class

--- backend/orders/models.py
+++ backend/orders/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from customers.models import Customer
 # Create your models here.
+
+class Order(models.Model):
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    order_date = models.DateField()
