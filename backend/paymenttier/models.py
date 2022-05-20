668a1b47f9895c74ad9bee3926a4dfe91277abce
Alex <thatsoalex@gmail.com>
2022-05-20 15:47:35 -0500

    Created PaymentTier class

--- backend/paymenttier/models.py
+++ backend/paymenttier/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from customers.models import Customer
+from orders.models import Order
 # Create your models here.
+
+class PaymentTier(models.Model):
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
+    tier_one = models.CharField(max_length=255)
+    tier_two = models.CharField(max_length=255)
+    tier_three = models.CharField(max_length=255)
