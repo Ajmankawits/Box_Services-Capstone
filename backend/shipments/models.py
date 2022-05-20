e9f8a7aaec0a55aa81f0d59c62496b780b25045a
Alex <thatsoalex@gmail.com>
2022-05-20 13:50:29 -0500

    Created Shipment class

--- backend/shipments/models.py
+++ backend/shipments/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from orders.models import Order
 # Create your models here.
+
+class Shipment(models.Model):
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
+    shipment_date = models.DateField()
